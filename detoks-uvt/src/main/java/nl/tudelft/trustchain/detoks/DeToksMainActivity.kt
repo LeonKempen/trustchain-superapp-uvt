ddda4ae232c77b5f1aaf80ed7e57901247a62185
Nektarios Evangelou <nekkieee@hotmail.com>
2023-04-05 16:14:44 +0200

    Fixed the dependency error, but introduced another error when saving to firebase

--- detoks-uvt/src/main/java/nl/tudelft/trustchain/detoks/DeToksMainActivity.kt
+++ detoks-uvt/src/main/java/nl/tudelft/trustchain/detoks/DeToksMainActivity.kt
@@ -1,6 +1,9 @@
 package nl.tudelft.trustchain.detoks
 
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
 import nl.tudelft.trustchain.common.BaseActivity
 
 class DeToksActivity : BaseActivity() {
@@ -10,5 +13,18 @@ class DeToksActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         val actionBar = supportActionBar
         actionBar!!.hide()
+
+        val db = Firebase.firestore
+
+        val city = hashMapOf(
+            "name" to "Los Angeles",
+            "state" to "CA",
+            "country" to "USA"
+        )
+
+        db.collection("cities").document("LA")
+            .set(city)
+            .addOnSuccessListener { Log.d("Firestore", "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w("Firestore", "Error writing document", e) }
     }
 }
